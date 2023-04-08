b7e37803f2f24005c9a633a925de7106fe044b47
rosieyeon <sseyeon956@gmail.com>
2023-04-08 15:42:17 +0900

    code: solved boj 10807 개수세기

---
+++ sds-pro/basics/BOJ_10807_개수세기.java
@@ -0,0 +1,26 @@
+package com.basics;
+
+import java.util.Scanner;
+
+public class BOJ_10807_개수세기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i=0; i<n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int v = sc.nextInt();
+
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            if (arr[i] == v) {
+                cnt += 1;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
