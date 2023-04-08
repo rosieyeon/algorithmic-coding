0bc1a44b275833cde124f02aa063cd3551d156e6
rosieyeon <sseyeon956@gmail.com>
2023-04-08 15:40:32 +0900

    code: solved boj 01000 A+B

---
+++ sds-pro/basics/BOJ_01000_AB.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+import java.util.Scanner;
+
+public class BOJ_01000_AB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a + b);
+    }
+}
